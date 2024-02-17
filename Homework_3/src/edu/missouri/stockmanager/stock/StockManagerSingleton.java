044b6bcdda1ee06bea835f4178970b4b0c292778
jljcb <jljcb@DellXPS13>
2024-02-16 22:59:31 -0600

    added methods provided by Ethan

--- Homework_3/src/edu/missouri/stockmanager/stock/StockManagerSingleton.java
+++ Homework_3/src/edu/missouri/stockmanager/stock/StockManagerSingleton.java
@@ -67,49 +67,134 @@ public class StockManagerSingleton
 		return false;
 	}
 	
-	public boolean addItem(MediaProduct product)
-	{
-		return products.add(product);
-	}
-	
-	public boolean removeItem(MediaProduct product)
-	{
-		//TODO
-		return false;
-	}
-	
-	public boolean saveStock()
-	{
-		//TODO
-		return false;
-	}
-	
-	public void printListOfMediaProduct(ArrayList<MediaProduct> productList)
-	{
-		//TODO
-	}
-	
-	public ArrayList<MediaProduct> getMediaProductBelowPrice(int maxPrice)
-	{
-		//TODO
-		return new ArrayList<MediaProduct>();
-	}
-	
-	public ArrayList<VinylRecordProduct> getVinylRecordList(ArrayList<MediaProduct> productList)
-	{
-		//TODO
-		return new ArrayList<VinylRecordProduct>();
-	}
-	
-	public ArrayList<CDRecordProduct> getCDRecordsList(ArrayList<MediaProduct> productList)
-	{
-		//TODO
-		return new ArrayList<CDRecordProduct>();
-	}
-	
-	public ArrayList<TapeRecordProduct> getTapeRecordList(ArrayList<MediaProduct> productList)
-	{
-		//TODO
-		return new ArrayList<TapeRecordProduct>();
-	}
-}
+            public boolean updateItemPrice(MediaProduct product, double newPrice)
+        	{
+        		if(products != null) {
+        			for(MediaProduct existingProduct : products) {
+        				if (existingProduct.equals(product)) {
+        					existingProduct.setPrice(newPrice);
+        					return true;
+        				}
+        			}
+        		}
+        		return false;
+        	}
+        	
+        	public boolean addItem(MediaProduct product)
+        	{
+        		if (products != null && !products.contains(product)) {
+        			products.add(product);
+        		System.out.println("Successfully added product!");
+        		return true;
+        		}
+        		else {
+        			return false;
+        		}
+        	}
+        	
+        	public boolean removeItem(MediaProduct productToRemove)
+        	{
+        		if(products != null) {
+        			for (Iterator<MediaProduct> iterator = products.iterator(); iterator.hasNext();) {
+        				MediaProduct currentProduct = iterator.next();
+        				if(currentProduct.getTitle().equals(productToRemove.getTitle())) {
+        					iterator.remove();
+        					return true;
+        				}
+        			}
+        		}
+        			
+        		return false;
+        	}
+        	
+        	public boolean saveStock()
+        	{
+        		try(BufferedWriter writer = new BufferedWriter(new FileWriter(inventoryFilePath))){
+        			if (products != null) {
+        				for(MediaProduct product : products) {
+        					writer.write(product.toString());
+        					writer.newLine();
+        				}
+        			}
+        			System.out.println("Successfully Saved!");
+        			return true;
+
+        			
+        		}catch(IOException e){
+        			System.out.println("Save failed.");
+        			e.printStackTrace();
+        			return false;
+        		}
+        	}
+        	
+        	public void printListOfMediaProduct(ArrayList<MediaProduct> productList, Class<? extends MediaProduct> productType)
+        	{
+        		if(productList != null) {
+        			for(MediaProduct product :productList) {
+        				if( productType.isInstance(product)) {
+        					System.out.println("Title: " + product.getTitle() + ", Price: " + product.getPrice()
+        									+ ", Year: " + product.getYear() + ", Genre: " + product.getGenre());
+        				}
+        			}
+        		}
+        	}
+        	
+        	public ArrayList<MediaProduct> getMediaProductBelowPrice(int maxPrice)
+        	{
+        		ArrayList<MediaProduct> belowPriceProducts = new ArrayList<>();
+        		if(products != null) {
+        			for(MediaProduct product : products) {
+        				if(product.getPrice() <maxPrice) {
+        					belowPriceProducts.add(product);
+        				}
+        			}
+        		}
+        		return new ArrayList<MediaProduct>();
+        	}
+        	
+        	public ArrayList<VinylRecordProduct> getVinylRecordList(ArrayList<MediaProduct> productList)
+        	{
+        		ArrayList<VinylRecordProduct> vinylRecords = new ArrayList<>();
+        		if (productList != null) {
+        			for (MediaProduct product: productList) {
+        				if(product instanceof VinylRecordProduct) {
+        					vinylRecords.add((VinylRecordProduct) product);
+        				}
+        			}
+        		}
+        			
+        		
+        		return new ArrayList<VinylRecordProduct>();
+        	}
+        	
+        	public ArrayList<CDRecordProduct> getCDRecordsList(ArrayList<MediaProduct> productList)
+        	{
+        		ArrayList<CDRecordProduct> CDRecords = new ArrayList<>();
+        		if (productList != null) {
+        			for (MediaProduct product: productList) {
+        				if(product instanceof CDRecordProduct) {
+        					CDRecords.add((CDRecordProduct) product);
+        				}
+        			}
+        		}
+        			
+        		
+        		return new ArrayList<CDRecordProduct>();
+        	}
+        	
+        	public ArrayList<TapeRecordProduct> getTapeRecordList(ArrayList<MediaProduct> productList)
+        	{
+        		ArrayList<TapeRecordProduct> tapeRecords = new ArrayList<>();
+        		if (productList != null) {
+        			for (MediaProduct product: productList) {
+        				if(product instanceof TapeRecordProduct) {
+        					tapeRecords.add((TapeRecordProduct) product);
+        				}
+        			}
+        		}
+        			
+        		
+        		return new ArrayList<TapeRecordProduct>();
+        	}
+        }
+       
